b06e8eef10be8048bf07d8dbccaa30d5a0bf2804
Nuggit40 <58763538+Nuggit40@users.noreply.github.com>
2021-03-23 11:29:24 -0400

    removed old files

--- signUp.js
+++
@@ -1,6 +0,0 @@
-const SignUpButton = document.getElementById("signUp-form-submit");
-SignUpButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    window.location.href = "SignUp.html";
-
-})
